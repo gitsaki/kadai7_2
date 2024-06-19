99d927a73eb19d350edf6989360d5eae20b8b7d4
git <sakichanpoppo3609@gamil.com>
2024-06-19 09:32:21 +0900

    create preparation kadai7_2

--- kadai7_2.py
+++ kadai7_2.py
@@ -1,6 +1,6 @@
 import random
 
-def randomnumber():
+def random_number():
     return random.rondint(100,999)
 
 def input():
@@ -9,4 +9,13 @@ def input():
         return ask
     else:
         print("Please enter a 3-digit number.")
-  
+
+def feedback(random_number, ask):
+    if ask == random_number:
+        print("Correct!")
+    else:
+        if ask < random_number:
+            print("Hint: The correct number is higher.")
+        else:
+            print("Hint: The correct number is lower.")
+            
